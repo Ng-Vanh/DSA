6c37f9f2102da5e1fa54c503f6fb7c29d641bddf
Ng-Vanh <nguyenvietanh2234@gmail.com>
2023-08-28 16:18:25 +0700

    week1

--- src/week1/bai1.java
+++ src/week1/bai1.java
@@ -5,3 +5,16 @@ public class bai1 {
         System.out.println("Hello, World");
     }
 }
+/**
+ * dung cmd: + javac bai1.java để biên dịch
+ *           + java bai1.java để chay
+ *
+ * Bai 4: - java bai1:
+                 Error: Could not find or load main class bai1
+                 Caused by: java.lang.NoClassDefFoundError: bai1 (wrong name: week1/bai1)
+        - java bai1 @! nhuw tren
+        - java bai1.class Bob ==> Error: Could not find or load main class bai1.class
+                                            Caused by: java.lang.ClassNotFoundException: bai1.class
+        -
+ *
+ * **/
---
+++ src/week1/bai6.java
@@ -0,0 +1,25 @@
+package week1;
+
+public class bai6 {
+    public static void main(String[] args) {
+        System.out.println("***        **    **               **       *");
+        System.out.println("** **      **     **             **      ** **");
+        System.out.println("**  **     **      **           **      **   **");
+        System.out.println("**   **    **       **         **      **     **");
+        System.out.println("**    **   **        **       **      ***********");
+        System.out.println("**     **  **         **     **      **         **");
+        System.out.println("**      ** **          **   **      **           **");
+        System.out.println("**       ****           ** **      **             **");
+        System.out.println("**        ***             *       **               **");
+    }
+}
+
+/*
+* Bai7: khi bien dich chuong trinh:
+*   public class Hello {
+        public static void main() {
+            System.out.println("Doesn't execute");
+         }
+    }
+    * chuong trình vẫn in ra màn hình doesn't execute
+* */
